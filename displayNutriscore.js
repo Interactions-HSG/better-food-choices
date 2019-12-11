7afc9c5f587b81f3c1fa36ff9221138be716306d
Klaus Fuchs <klaus.l.fuchs@gmail.com>
2019-12-11 17:54:45 +0100

    restore old displaynutriscore

--- displayNutriscore.js
+++ displayNutriscore.js
@@ -1,361 +1,3 @@
-// Checking if running properly.
-console.log("Chrome extension is working!");
-
-var group;
-var group = localStorage.getItem("GroupName");
-console.log ('group at refresh'+ group)
-
-// Admin Section.
-chrome.runtime.onMessage.addListener(
-    function (request){
-    if (request.Notice) {
-        console.log("The Extension button was clicked");
-        
-        document.documentElement.style.height = "100%";
-        document.body.style.height = "100%";
-        document.documentElement.style.width = '100%';
-        document.body.style.width = '100%';
-
-        var div2 = document.createElement("DIV");
-        var form = document.createElement("FORM");
-        var title = document.createElement("TEXT");
-        var explanation = document.createElement("TEXT");
-        var frag = document.createDocumentFragment();
-        var input = document.createElement("INPUT");
-        var confirm = document.createElement("INPUT");
-        var closer = document.createElement("INPUT");
-        var nutriInfo = document.createElement("INPUT");
-        var Adminpassword = document.createElement("INPUT");
-        var finishbutton = document.createElement("INPUT");
-
-        var country_germany = document.createElement("INPUT");
-        var country_ch = document.createElement("INPUT");
-        
-        var group_a = document.createElement("INPUT");
-        var group_b= document.createElement("INPUT");
-        var group_c= document.createElement("INPUT");
-
-
-        // Only append the elements if they are not already open.
-       
-        if ($("#overlay").length == 0) {
-            document.body.appendChild(div2);
-            div2.appendChild(form);
-            form.appendChild(title);
-            form.appendChild(explanation);
-            form.appendChild(input);
-            form.appendChild(closer);
-            form.appendChild(nutriInfo);
-            form.appendChild(finishbutton)
-            
-            
-
-
-        } else if ($("#overlay").length > 0) {
-            console.log("The menu is already open");
-        }
-        
-        form.style.textAlign = "center";
-
-        div2.id = "overlay";
-        div2.style.position = 'fixed';
-        div2.style.top = '30%';
-        div2.style.left = '10%';
-        div2.style.width = '78%';   
-        div2.style.height = '50%';
-        div2.style.zIndex = '100';
-        div2.style.opacity = '0.95';
-        div2.style.backgroundColor = 'white';
-        div2.style.border = 'solid';
-        div2.style.borderWidth = '5px';
-        div2.style.borderRadius = '20px';
-        div2.style.borderColor = 'lightblue';
-
-        title.id = "title"
-        title.textContent = "Welcome to the Nutriscore Study";
-        title.style.fontSize = "20px";
-        title.style.position = "absolute";
-        title.style.top = "7%";
-        title.style.left = "40%";
-
-        explanation.textContent = "TEXT"
-        explanation.style.fontSize = "14px";
-        explanation.style.position = "absolute";
-        explanation.style.top = "53%"
-        explanation.style.left = "19%";
-
-        input.id = "Adminbutton";
-        input.type = "button";
-        input.value = "Admin";
-        input.style.position = "absolute";
-        input.style.top = "39%";
-        input.style.left = "42%";
-        input.style.color = "darkgreen";
-        input.style.backgroundColor = "lightgreen";
-
-
-        
-
-
-
-    ////Admin section
-
-       
-        $("#Adminbutton").on("click", function() {
-    
-        var password2 = '123';
-
-
-            Adminpassword.id = "Adminpass";
-            Adminpassword.type = "Text";
-            Adminpassword.value = "Password";
-            Adminpassword.style.position = "absolute";
-            Adminpassword.style.top = "60%";
-            Adminpassword.style.left = "42%";
-            Adminpassword.style.color = "darkgreen";
-            Adminpassword.style.backgroundColor = "lightgreen";
-
-            confirm.id = "confirm";
-            confirm.type = "button";
-            confirm.value = "Confirm";
-            confirm.style.position = "absolute";
-            confirm.style.top = "60%";
-            confirm.style.left = "60%";
-            confirm.style.color = "darkgreen";
-            confirm.style.backgroundColor = "lightgreen";
-
-            form.appendChild(Adminpassword);
-            form.appendChild(confirm);
-//Password 
-            $("#confirm").on("click", function() {
-
-               password2 = document.getElementById("Adminpass").value;
-            
-
-            console.log(" Written password is : " + password2);
-//Set Password here 
-            if (password2 == '123') {
-
-                if ($("#country_germany").length == 0 && $("#group_a").length == 0) {
-                    frag.appendChild(country_germany);
-                    frag.appendChild(country_ch);
-                    frag.appendChild(group_a);
-                    frag.appendChild(group_b);
-                    frag.appendChild(group_c);
-                    form.appendChild(frag);
-                    
-                    
-                
-
-
-                country_germany.id = "country_germany";
-                country_germany.type = "button";
-                country_germany.value = "GERMANY";
-                country_germany.style.position = "absolute";
-                country_germany.style.top = "20%";
-                country_germany.style.left = "60%";
-                country_germany.style.color = "red";
-                country_germany.style.backgroundColor = "lightred";
-
-                $("#country_germany").on("click", function() {
-
-                    var Country = "Germany";
-                    localStorage.setItem("CountryName", Country);
-                 
-     
-                 console.log(" Country Selected is  : " + Country );
-
-                
-                });
-
-                country_ch.id = "country_ch";
-                country_ch.type = "button";
-                country_ch.value = "Switzerland";
-                country_ch.style.position = "absolute";
-                country_ch.style.top = "20%";
-                country_ch.style.left = "80%";
-                country_ch.style.color = "red";
-                country_ch.style.backgroundColor = "lightred";
-
-                $("#country_ch").on("click", function() {
-
-                    Country = "Switzerland";
-                    localStorage.setItem("CountryName", Country );
-                 
-     
-                 console.log(" Country Selected is  : " + Country);
-
-                
-                });
-
-                group_a.id = "group_a";
-                group_a.type = "button";
-                group_a.value = "NutriScore ABCDE";
-                group_a.style.position = "absolute";
-                group_a.style.top = "90%";
-                group_a.style.left = "20%";
-                group_a.style.color = "blue";
-                group_a.style.backgroundColor = "lightblue";
-
-                group_b.id = "group_b";
-                group_b.type = "button";
-                group_b.value = "NutriScore AB";
-                group_b.style.position = "absolute";
-                group_b.style.top = "90%";
-                group_b.style.left = "50%";
-                group_b.style.color = "blue";
-                group_b.style.backgroundColor = "lightblue";
-
-
-                group_c.id = "group_c";
-                group_c.type = "button";
-                group_c.value = "NutriScore Disabled";
-                group_c.style.position = "absolute";
-                group_c.style.top = "90%";
-                group_c.style.left = "80%";
-                group_c.style.color = "blue";
-                group_c.style.backgroundColor = "lightblue";
-
-
-
-                $("#group_a").on("click", function() {
-
-                    var group = "A";
-                    localStorage.setItem("GroupName", group);
-                 
-     
-                 console.log(" Group selected is  : " + group );
-
-                
-                });
-
-                
-                $("#group_b").on("click", function() {
-
-                    var group = "B";
-                    localStorage.setItem("GroupName", group);
-                 
-     
-                    console.log(" Group selected is  : " + group );
-
-                
-                });
-
-                
-                $("#group_c").on("click", function() {
-
-                    var group = "C";
-                    localStorage.setItem("GroupName", group);
-                 
-     
-                    console.log(" Group selected is  : " + group );
-
-                
-                });
-
-
-
-
-
-
-
-                
-                
-                
-            
-                } else if ($("#country_ch").length > 0) {
-                    console.log("Admin menu is already open");
-                }
-            } else {
-                console.log(" Password False");
-            };
-    });
-});
-        
-
-
-
-        nutriInfo.id = "nutriInfo";
-        nutriInfo.type = "button";
-        nutriInfo.value = "More infos about Nutriscore";
-        nutriInfo.style.position = "absolute";
-        nutriInfo.style.top = "84%";
-        nutriInfo.style.left = "76%";
-        $("#nutriInfo").on("click", function() {
-            window.open("https://world.openfoodfacts.org/nutriscore");
-        
-    
-        });
-
-        var alternative1 = document.createElement("INPUT");
-        var alternative2 = document.createElement("INPUT");
-        var alternative3 = document.createElement("INPUT");
-
-        alternative1.id = "alt1";
-        alternative1.type = "button";
-        alternative1.style.position = "absolute";
-        alternative1.style.top = "65%";
-        alternative1.style.left = "10%";
-        alternative1.style.border = "none";
-        alternative1.style.color = "blue";
-        alternative1.style.textDecoration = "underline";
-        alternative1.value = "Alternative 1";
-
-        alternative2.id = "alt2";
-        alternative2.type = "button";
-        alternative2.style.position = "absolute";
-        alternative2.style.top = "75%";
-        alternative2.style.left = "10%";
-        alternative2.style.border = "none";
-        alternative2.style.color = "blue";
-        alternative2.style.textDecoration = "underline";
-        alternative2.value = "Alternative 2";
-
-        alternative3.id = "alt3";
-        alternative3.type = "button";
-        alternative3.style.position = "absolute";
-        alternative3.style.top = "85%";
-        alternative3.style.left = "10%";
-        alternative3.style.border = "none";
-        alternative3.style.color = "blue";
-        alternative3.style.textDecoration = "underline";
-        alternative3.value = "Alternative 3";
-        
-
-        closer.id = "exitButton";
-        closer.type = "button";
-        closer.value = "Close";
-        closer.style.position = "absolute";
-        closer.style.top = "5%";
-        closer.style.left = "91%";
-        
-        $("#exitButton").on("click", function() {
-            $(document.body).children("#overlay").remove();
-            location.reload();
-            
-        });
-
-        finishbutton.id = "finishbutton";
-        finishbutton.type = "button";
-        finishbutton.value = "Finish study";
-        finishbutton.style.position = "absolute";
-        finishbutton.style.top = "45%";
-        finishbutton.style.left = "42%";
-        finishbutton.style.color = "lightred";
-        finishbutton.style.backgroundColor = "darkred";
-
-        $("#finishbutton").on("click", function() {
-        console.log ('Study is finished')
-        
-
-        
-
-        });
-    }
-})
-
-
-
 // Looking for unique identifier(GTIN).
 
 let identifierBox = document.getElementsByClassName("mui-panel-body additional-information-panel-body");
@@ -382,7 +24,6 @@ if (gtinTxt.length > 14) {
     var gtin = parseInt(gtinTxt);
 }
 console.log("The GTIN of this product is: " + gtin);
-
 
 // Looking for categorization data.
 let categorizationTable = document.getElementsByClassName("mui-breadcrumb");
@@ -419,15 +60,12 @@ if (categorization.indexOf("joghurt & joghurtdrinks") >= 0) {
 } else if (categorization.indexOf("joghurt & joghurtdrinks") <0) {
     joghurtCheck = false;
 }
-
-// Checking if it belongs to the Cheese/Kase category.
 var kaseCheck;
 if (categorization.indexOf("Käse ") >= 0) {
     kaseCheck = true;
 } else if (categorization.indexOf("Käse ") <0) {
     kaseCheck = false;
 }
-
 
 // Function for calling API.
 // If no data is available, API gets a notification in his system.
@@ -438,13 +76,11 @@ var url = "https://eatfit-service.foodcoa.ch/products/" + gtin + "/?resultType=a
 var API;
 var nutri_score_final;
 
-
 // Testing if API is offering the data.
 function productDataCallback(json)
 {
     if (json["products"] == null) {
         console.log("The product is not availabe in the database.");      
-
     } else {
         console.log("Product data is available in API.");
         // console.log(json["products"][0].nutri_score_final);
@@ -468,8 +104,6 @@ function loadProductData(callback) {
             if(typeof callback == "function") {
                  nutri_score_final = callback(json);
                  show (nutri_score_final);
-                 
-
 
             }
         },
@@ -477,7 +111,6 @@ function loadProductData(callback) {
         error:function(textStatus, errorThrown) {
             console.log(textStatus, errorThrown);
         },
-
     });
 }
 //function to display nutri-score when the page starts
@@ -879,22 +512,11 @@ function getScoreLocal() {
                     if (fruitvegetablesScore>=5) {
                     nutriScoreNumber = badIngredientScore-goodIngredientScore-fruitvegetablesScore-fibersScore;
                     }
-                    else{ 
-                        if (kaseCheck==true){
-
-                            nutriScoreNumber = badIngredientScore-goodIngredientScore-fruitvegetablesScore-fibersScore;
-                        }
-                        else{
-
+                    else{
                         nutriScoreNumber = badIngredientScore-fruitvegetablesScore-fibersScore;
-                        }
                     }
                    }
                 
-
-
-                
-
                 if (food === true) {
                     if (nutriScoreNumber <= -1) {
                         nutriScore = "A";
@@ -929,52 +551,13 @@ function getScoreLocal() {
             return nutriScore;
 
         }
-//Retrun error if the local NutriScore is different from API's Nutriscore.
-     
-if (nutriScore==nutri_score_final){
-    var xhr = new XMLHttpRequest();
-     var url2 = "https://eatfit-service.foodcoa.ch/product/report/";
-     xhr.setRequestHeader ("Authorization", "Basic " + btoa(username + ":" + password));
-     xhr.open("POST", url2, true);
-     xhr.setRequestHeader("Content-Type", "application/json");
-     xhr.onreadystatechange = function () {
-     if (xhr.readyState === 4 && xhr.status === 200) {
-        var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://eatfit-service.foodcoa.ch/product/report/",
-            "method": "POST",
-            "headers": {
-              "Content-Type": "application/json",
-              "cache-control": "no-cache",
-              "Postman-Token": "010c1d65-2b2e-4558-8431-1c6ef39c46ba"
-            },
-            "processData": false,
-            "data": "{\"gtin\":\"123123\", \"app\": \"TestApp\", \"error_description\": \"I am just here for the testing\"}"
-          }
-          
-          $.ajax(settings).done(function (response) {
-            console.log(response);
-          });
-          
-        var json = JSON.parse(xhr.responseText);
-  
-     }
-     };
-     var data = JSON.stringify({ "GTIN": gtin, "app": "ch.autoidlabs.nutriscore_chrome_extension","error_description": "Nutri-Score seems wrong. Please check"},);
-     xhr.send(data);
-
-
-}
-
-
+//function to return nutrisocre 
 
 if (group == "A"){
-
-function show(nutri_score_final){
-    nutriScore = nutri_score_final;
+function show(_nutri_score_final){
+    nutriScore = _nutri_score_final;
     if(nutriScore==null){
-        console.log("Nutriscore is Null")
+        console.log("Here you go")
         nutriScore = getScoreLocal();
     }
     console.log(nutriScore);
@@ -1026,8 +609,8 @@ function show(nutri_score_final){
 }
 }
 else if (group == "B") {
-    function show(nutri_score_final){
-        nutriScore = nutri_score_final;
+    function show(_nutri_score_final){
+        nutriScore = _nutri_score_final;
         if(nutriScore==null){
             console.log("Here you go")
             nutriScore = getScoreLocal();
@@ -1054,11 +637,11 @@ else if (group == "B") {
             img.src = nsBURL;
             queryname = "NutriscoreB"
         } else if (nutriScore === "C") {
-            console.log("NutriScore image Removed!");
+            
         } else if (nutriScore === "D") {
-            console.log("NutriScore image Removed!"); 
+            
         } else if (nutriScore === "E") {
-            console.log("NutriScore image Removed!"); 
+            
         } else {
             img.src = nsVURL;
             queryname = "NutriscoreV"
@@ -1074,18 +657,12 @@ else if (group == "B") {
     
         position.insertBefore(div, position.childNodes[0]);
       //  loadQueryResults(queryname, queryResultsCallback);
-
-
 }
 }
 else if (group == "C") {
-
         console.log("NutriScore image Removed!");
-    
     }
     }
     else {
         console.log("NutiScore Group Error");
 }
-
-
